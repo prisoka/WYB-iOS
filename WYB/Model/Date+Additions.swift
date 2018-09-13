c3ed515a294cf4cb19efd78ca937338d6591899b
Priscilla Okawa <prisoka@gmail.com>
2018-09-13 14:08:20 -0700

    wip: date string formatting

--- WYB/Model/Date+Additions.swift
+++ WYB/Model/Date+Additions.swift
@@ -8,10 +8,31 @@
 
 import Foundation
 
+//extension String {
+//    func toDate() -> Date? {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"
+//        return dateFormatter.date(from: self)
+//    }
+//}
+
 extension String {
-    func toDate() -> Date? {
+    func convertDateFormatter(date: String) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"
-        return dateFormatter.date(from: self)
+        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"//string date format
+        dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+        dateFormatter.locale = Locale(identifier: "en_US")
+        let convertedDate = dateFormatter.date(from: self)
+
+        guard dateFormatter.date(from: date) != nil else {
+            assert(false, "no date from string")
+            return ""
+        }
+
+        dateFormatter.dateFormat = "MM'-'dd'-'yyyy"// format I want to convert
+        dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+        let timeStamp = dateFormatter.string(from: convertedDate!)
+        print(timeStamp)
+        return timeStamp
     }
 }
